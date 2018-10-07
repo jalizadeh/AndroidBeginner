0bda2db393537f2514e29ed600703799d30a9bd1
Javad <jalizade3d@yahoo.com>
2018-10-07 17:29:09 +0200

    ListView

--- app/src/main/java/jalizadeh/com/androidbeginner/MainActivity.java
+++ app/src/main/java/jalizadeh/com/androidbeginner/MainActivity.java
@@ -5,12 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -81,6 +86,25 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "onClick: 2nd activity run");
             }
         });
+
+
+        //video #7
+        ListView list = (ListView) findViewById(R.id.list1);
+
+        ArrayList<String> name = new ArrayList<>();
+        name.add("Javad");
+        name.add("Farzaneh");
+        name.add("Mr. Alex");
+        name.add("Ms. Sera");
+
+        //use one of the default layoutss
+        //ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, name);
+
+        //OR, use a custom design for list
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.my_list_layout, name);
+        list.setAdapter(adapter);
+
+
     }
 
 
