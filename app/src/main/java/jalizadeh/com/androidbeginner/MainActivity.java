157a421eccfaa212b9da09ded938f16d15268868
Javad <jalizade3d@yahoo.com>
2018-10-09 09:38:26 +0200

    Lifecycle - #15

--- app/src/main/java/jalizadeh/com/androidbeginner/MainActivity.java
+++ app/src/main/java/jalizadeh/com/androidbeginner/MainActivity.java
@@ -23,11 +23,54 @@ public class MainActivity extends AppCompatActivity {
     //type "logt" here, then get this line
     private static final String TAG = "MainActivity";
 
+    //video #15
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");;
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+    //end video #15
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate: Application Started.");
+        Log.d(TAG, "onCreate: Application created.");
 
         //video #2 - TextView
         TextView tv1 = (TextView) findViewById(R.id.textView1);
@@ -183,6 +226,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+
+
+
     }
 
 
